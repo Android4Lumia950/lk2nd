da1e99cfb22f6e7c679ccd80da4e7553e3747ce9
djokcho <140524342+djokcho@users.noreply.github.com>
2024-10-31 20:41:31 +0200

    Update panel_duke_cmd.h

--- dev/gcdb/display/include/panel_duke_cmd.h
+++ dev/gcdb/display/include/panel_duke_cmd.h
@@ -238,6 +238,7 @@ static struct labibb_desc duke_wqhd_dualdsi_cmd_labibb = {
 	3, //pwr_up_delay
 	3, //pwr_down_delay
 	1  //ibb_discharge_en
+        0 //swire_control
 };
 
 #endif /*_PANEL_DUKE_WQHD_DUALDSI_CMD_H_*/
